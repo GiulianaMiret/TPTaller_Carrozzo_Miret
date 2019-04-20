601c70cb6ebfb1ea27cb3b0c5a234b95cc2cf010
Giuliana Dariela Miret <dariela_77@hotmail.com>
2019-04-20 20:41:56 -0300

    Hice un proyecto para testear la fachada (y otras coasas) pero todavia no tiene nada por problemas técicos jaja

---
+++ FachadaTest/FachadaTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Controlador;
+
+namespace FachadaTest
+{
+    [TestClass]
+    public class FachadaTest
+    {
+        [TestMethod]
+        public void InsertarBannerTest()
+        {
+            
+        }
+    }
+}
---
+++ FachadaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FachadaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FachadaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6606e1b8-b932-47d4-9063-842e7b2decb1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
