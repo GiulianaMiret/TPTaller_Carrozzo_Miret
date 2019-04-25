30607b8a8854cedc1751cf36172b56a703cab140
Eduardo Carrozzo <carrozzo.eduardo@gmail.com>
2019-04-24 23:37:38 -0300

    ninject

--- Vista/InjectModule.cs
+++ Vista/InjectModule.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ninject;
+using EntityFramework.Services;
+using Core.Models;
 
 namespace Vista
 {
@@ -13,8 +15,10 @@ namespace Vista
         public override void Load()
         {
             //Bindeamos las inyecciones de dependencias y las hacemos Singleton , es decir, una sola instancia de las variables para toda la ejecucion del programa.
-            //Bind<IRepository>().To<BannerRepository>().InSingletonScope();
-            //Bind<IRepository>().To<CampaniaRepository>().InSingletonScope();
+            Bind<IRepository<Banner>>().To<BannerRepository>().InSingletonScope();
+            Bind<IRepository<Campania>>().To<CampaniaRepository>().InSingletonScope();
+            Bind<IRepository<Fuente>>().To<FuenteRepository>().InSingletonScope();
+            Bind<IRepository<Imagen>>().To<ImagenRepository>().InSingletonScope();
         }
     }
 }
